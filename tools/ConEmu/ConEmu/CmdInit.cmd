33bfedddbcd07d645d9d0b398cf8ec2effbbb082
Man, Jianting (Meco) <920369182@qq.com>
2022-10-22 00:58:44 -0400

    Update CmdInit.cmd

--- tools/ConEmu/ConEmu/CmdInit.cmd
+++ tools/ConEmu/ConEmu/CmdInit.cmd
@@ -70,7 +70,8 @@ start /b python %~dp0..\..\scripts\init_env.py
 :: ====== RT-Thread ENV Change Code Page ================
 
 chcp 65001 > nul
-echo RT-Thread Env Tool for Windows (V1.3.5)
+echo RT-Thread Env Tool for Windows 
+env -v
 echo  ^\ ^| /
 echo - RT -     Thread Operating System
 echo  / ^| ^\
